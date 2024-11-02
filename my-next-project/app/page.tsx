d11849bdf21ee2c11f987f2d67ac479f6411d8b8
logintech12345 <logintech12345@gmail.com>
2024-11-02 22:02:35 +0900

    First Commit

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -2,11 +2,14 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return(
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+              私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
+          <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+        </div>
+      </section> 
   );
 }
