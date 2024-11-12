eaaa1df84db9383d017ca5cf2c1a2a1964c2d871
logintech12345 <logintech12345@gmail.com>
2024-11-13 07:14:20 +0900

    Add Header Page

---
+++ my-next-project/app/_components/Category/index.tsx
@@ -0,0 +1,11 @@
+import type { Category } from "@/app/_libs/microcms";
+import styles from "./index.module.css";
+import { type } from "os";
+
+type Props = {
+    category: Category;
+};
+
+export default function Category ({ category }: Props) {
+    return <span className={styles.tag}>{category.name}</span>
+}
