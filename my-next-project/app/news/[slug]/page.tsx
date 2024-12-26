2bed1be397f7bc68bffe140ab1ad2c4c482c99c1
logintech12345 <logintech12345@gmail.com>
2024-12-27 07:09:27 +0900

    7章完了

--- my-next-project/app/news/[slug]/page.tsx
+++ my-next-project/app/news/[slug]/page.tsx
@@ -8,11 +8,16 @@ type Props = {
     params: {
         slug: string;
     };
+    searchParams: {
+        dk?: string;
+    };
 };
 
-export default async function Page({params}: Props) {
+export default async function Page({params, searchParams}: Props) {
 
-    const data = await getNewsDetail(params.slug).catch(notFound);
+    const data = await getNewsDetail(params.slug, {
+        draftKey: searchParams.dk,
+    }).catch(notFound);
     return(
         <>
             <Article data={data} />
--- my-next-project/app/news/search/page.tsx
+++ my-next-project/app/news/search/page.tsx
@@ -1,18 +1,25 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import { NEWS_LIST_LIMIT } from "@/app/_components/_constants";
-import NewsList from "../_components/NewsList";
-import Pagenation from "@/app/_components/Pagination";
+import NewsList from "@/app/_components/NewsList";
 import SearchField from "@/app/_components/SearchField";
 
-export default async function Page() {
-    const {contents: news, totalCount} = await getNewsList({
+type Props = {
+    searchParams: {
+        q?: string;
+    };
+};
+
+export default async function  Page({searchParams}: Props) {
+    const { contents: news} = await getNewsList({
         limit: NEWS_LIST_LIMIT,
+        q: searchParams.q,
     });
+
     return (
         <>
             <SearchField />
             <NewsList news={news} />
-            <Pagenation totalCount={totalCount} />
         </>
-    );
+    )
+    
 }
