fe22b3ae0ceb678bd5cedcf844780353dc1555bf
logintech12345 <logintech12345@gmail.com>
2025-01-04 07:58:14 +0900

    ニュースにISRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,7 @@ import NewsList from "../_components/NewsList";
 import Pagenation from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
     const {contents: news, totalCount} = await getNewsList({
