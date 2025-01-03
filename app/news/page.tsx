c1842d6eae3ff81191cc895cc8f6475905849c62
logintech12345 <logintech12345@gmail.com>
2025-01-04 07:56:02 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import NewsList from "../_components/NewsList";
 import Pagenation from "@/app/_components/Pagination";
 import SearchField from "@/app/_components/SearchField";
 
+export const revalidate = 0;
+
 export default async function Page() {
     const {contents: news, totalCount} = await getNewsList({
         limit: NEWS_LIST_LIMIT,
